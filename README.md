a18000d4aa26980e0317b18451b751a99adccdf3
Adriano Moura <adrianodemoura@gmail.com>
2021-06-24 21:30:21 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## Script para deploy Automático
+
+### Considerações
+Este script irá pesquisar o texto em todas as tabelas do banco
+
+### Requisitos
+* Conexão com banco de dados
+
+* Texto a ser pesquisado
+
+### Configurações
+
+### Modo de usar
+```
+$ search-tables texto
+```
---
+++ search-tables.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+: '
+Este script localiza a tabela e a coluna que possui o texto.
+'
+
+# descobrindo o diretório completo
+DIR_SCRIPT=${PWD}"/"$(dirname $0)
+
+# parameters
+USER='aapm'
+PASSWORD='aapm'
+DATABASE='aapm'
+TEXTO=$1
+
+if [ ${#TEXTO} = 0 ]; then
+	echo "Texto inválido !"
+	exit
+fi
+
+CONECTION="mysql -u$USER -p$PASSWORD $DATABASE"
+
+TABLES=`$CONECTION -e"SHOW TABLES"`
+
+ARR_TABLES=$(echo $TABLES | tr " " "\n")
+
+for TABLE in $ARR_TABLES
+do
+	COLUMNS=`$CONECTION -e"SELECT column_name FROM information_schema.columns WHERE table_name='$TABLE' AND table_schema='$DATABASE'" | grep -v 'column_name'`
+	
+	ARR_COLUMNS=$(echo $COLUMNS | tr " " "\n")
+	
+	for COLUMN in $ARR_COLUMNS
+	do
+		RES=`$CONECTION -e"SELECT * FROM $DATABASE.$TABLE WHERE $COLUMN LIKE '%$TEXTO%'"`
+
+		if [ ! -z "$RES" ]; then
+			echo "achei em $TABLE na coluna $COLUMN"
+		fi
+	done
+done
